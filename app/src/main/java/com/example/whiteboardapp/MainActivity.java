63a84a5ebfddd7c04ebd965d828290131cbeeea9
admin <mankum08071998@gmail.com>
2020-10-17 11:57:14 +0530

    screen size error fixed

--- app/src/main/java/com/example/whiteboardapp/MainActivity.java
+++ app/src/main/java/com/example/whiteboardapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.whiteboardapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
 
 import me.panavtec.drawableview.DrawableView;
@@ -20,13 +21,18 @@ public class MainActivity extends AppCompatActivity {
 
         drawableView = (DrawableView) findViewById(R.id.drawableView);
 
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int height = displayMetrics.heightPixels;
+        int width = displayMetrics.widthPixels;
+
         config = new DrawableViewConfig();
         config.setStrokeColor(getResources().getColor(android.R.color.black));
         config.setShowCanvasBounds(true);
-        config.setMaxZoom(3.0f);
+        config.setMaxZoom(5.0f);
         config.setMinZoom(1.0f);
-        config.setCanvasHeight(1080);
-        config.setCanvasWidth(1920);
+        config.setCanvasHeight(height);
+        config.setCanvasWidth(width);
 
         drawableView.setConfig(config);
     }
